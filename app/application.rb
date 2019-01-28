15a61b994d1d754f832b483104252fa614dfc38b
Brent Ransom <sadumedusa@gmail.com>
2019-01-28 00:00:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,8 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-      if @@cart != 0
+      if @@cart.count != 0
+        
     else
       resp.write "Path Not Found"
     end
